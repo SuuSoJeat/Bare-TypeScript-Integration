abeab37dce4ce588f987617306fee75f871d02d4
SuuSoJeat <suusojeat@gmail.com>
2020-07-08 13:58:47 +0700

    Error commit

---
+++ BasicTypeScriptIntegration/scripts/app.ts
@@ -0,0 +1,14 @@
+﻿//import Vue from "vue";
+
+var vm = new Vue({
+    el: '#app',
+    data: function () {
+        return {
+            items: [
+                {
+                    name: "name"
+                }
+            ]
+        };
+    }
+})
---
+++ BasicTypeScriptIntegration/wwwroot/js/app.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+var vm = new Vue({
+    el: '#app',
+    data: function () {
+        return {
+            items: [
+                {
+                    name: "name"
+                }
+            ]
+        };
+    }
+});
+//# sourceMappingURL=app.js.map
